018ee3d107af346fb030e2fbe3f349756d227b3e
Pushkar Verma <pushkar753@gmail.com>
2023-09-09 19:14:45 +0530

    google login created

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 const app = express()
-
-
-
+import userRoutes from "./routes/user.js"
+import { connectPassport } from "./utils/Provider.js";
 dotenv.config({
     path: "./config/config.env"
 })
+
+connectPassport();
+
+
+app.use("/api/v1", userRoutes);
+
+
 export default app;
